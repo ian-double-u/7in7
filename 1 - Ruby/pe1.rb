d89a8f7a978ef37879b6b3657552aa100dd24ddb
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-09-06 13:35:27 -0400

    Update pe1.rb

--- 1 - Ruby/pe1.rb
+++ 1 - Ruby/pe1.rb
@@ -1,5 +1,3 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-z = (1..999).select{ |x| x % 3 == 0 or x % 5 == 0 }.collect
-
-puts "Ans = #{z.sum}"
+puts "Ans = #{(1..999).select{ |x| x % 3 == 0 or x % 5 == 0 }.collect.sum}"
