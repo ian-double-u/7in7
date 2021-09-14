5ce5827fa0a35ccdbe610a8499af2b447e86ddfa
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-09-14 18:21:15 -0400

    Create day1_self_study.pl

---
+++ 3 - Prolog/day1_self_study.pl
@@ -0,0 +1,9 @@
+wrote(hofstadter,geb).
+wrote(hofstadter,i_am_a_strage_loop).
+wrote(harari,sapiens).
+wrote(hayek,road_to_serfdom).
+wrote(aurelius,meditations).
+
+% ?- wrote(hofstadter,What).
+%What = geb ;
+%What = i_am_a_strage_loop.
