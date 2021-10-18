d95075bbe4061fdd8a61a87b1e060d6ab9dcf6f9
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-10-18 09:57:41 -0400

    Update day1_self_study.clj

--- 6 - Clojure/day1_self_study.clj
+++ 6 - Clojure/day1_self_study.clj
@@ -2,9 +2,7 @@
 
 (big "ian" 2)
 
-(def ct-map {"class clojure.lang.PersistentList" :list 
-														 "class clojure.lang.PersistentArrayMap" :map 
-														 "class clojure.lang.PersistentVector" :vector})
+(def ct-map {"class clojure.lang.PersistentList" :list "class clojure.lang.PersistentArrayMap" :map "class clojure.lang.PersistentVector" :vector})
 
 (defn collection-type [col] (ct-map (str (class col))))
 (collection-type [3 2 1])
