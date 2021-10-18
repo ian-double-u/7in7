8f248582ece3762da3b3e8b7b37928fff4f281eb
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-10-18 09:56:50 -0400

    Create day1_self_study.clj

---
+++ 6 - Clojure/day1_self_study.clj
@@ -0,0 +1,10 @@
+(defn big [st n] (> (count st) n))
+
+(big "ian" 2)
+
+(def ct-map {"class clojure.lang.PersistentList" :list 
+														 "class clojure.lang.PersistentArrayMap" :map 
+														 "class clojure.lang.PersistentVector" :vector})
+
+(defn collection-type [col] (ct-map (str (class col))))
+(collection-type [3 2 1])
